fa71149ea4e83e73e57738eafa2780e49ec6e819
Anrijs <ajargans@gmail.com>
2014-02-20 14:23:41 +0200

    Added rspec to project

---
+++ spec/controllers/opendays_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe OpendaysController do
+
+  describe "GET 'index'" do
+    it "returns http success" do
+      get 'index'
+     # response.should redirect_to 
+    end
+  end
+
+end
