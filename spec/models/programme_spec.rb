3d2b7c68e0bf54e17bcd6a4e5ffc366c645adc69
Anrijs <ajargans@gmail.com>
2014-02-27 14:14:31 +0200

    Programme: added model test

--- spec/models/programme_spec.rb
+++ spec/models/programme_spec.rb
@@ -1,5 +1,46 @@
 require 'spec_helper'
 
 describe Programme do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "relations" do
+    it {should belong_to(:faculty)}
+    it {should have_many(:openday_programmes)}
+  end
+
+  describe "validations" do
+    it {should validate_presence_of(:name).with_message(I18n.t('validation.name_presence'))}
+
+    context "when name in not unique for same faculty" do
+      it "returns error" do
+        pending "needs implementation"
+      end
+    end
+
+    context "when name same as other from different faculty" do
+      it "does not return error" do
+        pending "needs implementation"
+      end
+    end
+  end
+
+  describe "#create_slug" do
+    before do
+      @faculty = FactoryGirl.create(:faculty)
+    end
+
+    context 'when new record created' do
+      it "generates slug" do
+      	@programme = FactoryGirl.create(:programme, name: 'Pro & gramme', faculty_id: @faculty.id)
+        expect(@programme.slug).to eq('pro-gramme')
+      end
+    end
+
+    context 'when slug already in use' do
+      it "does not validate" do
+        pending "needs implementation"
+        FactoryGirl.create(:programme, name: 'Pro & gramme', faculty_id: @faculty.id)
+      	@programme = FactoryGirl.create(:programme, name: 'Pro - Gramme', faculty_id: @faculty.id)
+        expect(@programme.valid?).to eq(false)
+      end
+    end
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,12 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 
+if ENV['RAILS_ENV'] == 'test'
+  require 'simplecov'
+  SimpleCov.start 'rails'
+  puts "required simplecov"
+end
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
