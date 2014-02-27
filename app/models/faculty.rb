75b4951133b7a2f2eaa55778410c498409de37ba
Anrijs <ajargans@gmail.com>
2014-02-27 13:35:28 +0200

    Faculty: model tests added

--- app/models/faculty.rb
+++ app/models/faculty.rb
@@ -2,19 +2,19 @@ class Faculty < ActiveRecord::Base
   has_many :programmes, dependent: :destroy
   has_many :openday_faculties
 
-  validates_presence_of :name, message: I18n.t('validation_name_presence')
-  validates_uniqueness_of :name, message: I18n.t('validation_name_uniqueness')
+  validates_presence_of :name, message: I18n.t('validation.name_presence')
+  validates_uniqueness_of :name, message: I18n.t('validation.name_uniqueness')
 
-  validates_presence_of :short_name, message: I18n.t('validation_short_name_presence')
-  validates_uniqueness_of :short_name, message: I18n.t('validation_short_name_uniqueness')
+  validates_presence_of :short_name, message: I18n.t('validation.short_name_presence')
+  validates_uniqueness_of :short_name, message: I18n.t('validation.short_name_uniqueness')
   
-  validates_uniqueness_of :slug, message: I18n.t('validation_name_uniqueness')
+  validates_uniqueness_of :slug, message: I18n.t('validation.slug_uniqueness')
 
 
   before_validation :create_slug
 
 private
   def create_slug
-    self.slug = self.short_name.parameterize
+    self.slug = self.short_name.to_s.parameterize
   end
 end
--- spec/models/faculty_spec.rb
+++ spec/models/faculty_spec.rb
@@ -1,5 +1,33 @@
 require 'spec_helper'
 
 describe Faculty do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "relations" do
+    it {should have_many(:programmes)}
+    it {should have_many(:openday_faculties)}
+  end
+
+  describe "validations" do
+    it {should validate_presence_of(:name).with_message(I18n.t('validation.name_presence'))}
+    it {should validate_presence_of(:short_name).with_message(I18n.t('validation.short_name_presence'))}
+
+    it {should validate_uniqueness_of(:name).with_message(I18n.t('validation.name_uniqueness'))}
+    it {should validate_uniqueness_of(:short_name).with_message(I18n.t('validation.short_name_uniqueness'))}
+  end
+
+  describe "#create_slug" do
+    context 'when new record created' do
+      it "generates slug" do
+      	@faculty = FactoryGirl.create(:faculty, short_name: "T &e st  ")
+      	expect(@faculty.slug).to eq('t-e-st')
+      end
+    end
+
+    context 'when slug already in use' do
+      it "does not validate" do
+        FactoryGirl.create(:faculty, short_name: "T &e st  ")
+        @faculty = FactoryGirl.build(:faculty, short_name: "t E St")
+        expect(@faculty.valid?).to eq(false)
+      end
+    end
+  end
 end
