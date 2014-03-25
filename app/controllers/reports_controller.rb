57d5fa5f3a337532c6f370004e7d1cee1eaeb08f
Anrijs <ajargans@gmail.com>
2014-03-25 11:58:23 +0200

    100th commit! Reports: New queries for line graphs

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -10,14 +10,10 @@ class ReportsController < ApplicationController
     unless @openday
       @openday = Openday.find(params[:id])
     end
-
-    start_date = DateTime.parse(@openday.registration_open.to_s)
-    end_date = DateTime.parse(@openday.registration_end.to_s)
-
+  
     @faculty_registrations = @openday.report_registrations
     @openday_registrants = @openday.report_registrants
     @openday_countries = @openday.report_countries
-    @openday_range = []
   end
 
   def faculty
@@ -26,43 +22,19 @@ class ReportsController < ApplicationController
       @openday = Openday.find(params[:report_id])
     end
 
-    start_date = DateTime.parse(@openday.registration_open.to_s)
-    end_date = DateTime.parse(@openday.registration_end.to_s)
-
     @faculty = Faculty.find_by_slug(params[:id])
-    unless @openday
-      @faculty = Faculty.find(params[:id])
+    unless @faculty
+      @faculty = Faculty.find_by_id(params[:id])
     end
 
-    @openday_faculty = @openday.openday_faculties.find_by_faculty_id(@faculty.id)
+    if @faculty.nil?
+      redirect_to report_path(@openday)
+    else
+      @openday_faculty = @openday.openday_faculties.find_by_faculty_id(@faculty.id)
+      @programme_registrations = @openday_faculty.report_registrations
+      @faculty_registrants = @openday_faculty.report_registrants
 
-    @programme_registrations = {}
-    
-    @openday_range = []
-    @openday_registrations = []
-
-    registrations = @openday.registrations.where('faculty_id LIKE ?', @openday_faculty.faculty.id).select('DISTINCT(registrant_id)')
-    (start_date..end_date).each do |day|
-      @openday_range.push("#{day.strftime('%b %d')}")
-      unless DateTime.now < day
-        @openday_registrations.push registrations.where(
-          "created_at >= ? AND created_at < ?",
-              day.beginning_of_day, day.end_of_day).count
-      else
-        @openday_registrations.push(nil)
-      end
+      @openday_countries = @openday.report_countries
     end
-
-    registrants = @openday.registrants
-    @openday_countries = {}
-
-    registrations.each do |registration|
-      if @openday_countries[registration.registrant.country].nil?
-        @openday_countries[registration.registrant.country] = 0
-      end
-      @openday_countries[registration.registrant.country] = @openday_countries[registration.registrant.country] + 1
-    end
-
-    @openday_countries = (@openday_countries.sort_by { |name, count| count }).reverse
   end
 end
--- app/models/openday_faculty.rb
+++ app/models/openday_faculty.rb
@@ -17,4 +17,29 @@ class OpendayFaculty < ActiveRecord::Base
       return @ready
     end
   end
+
+  # Get all registrations for faculty grouped
+  #
+
+  def report_registrants
+    return Registration.select('*, count(DISTINCT registrant_id) as count, DAYOFYEAR(registrants.created_at) as date')
+          .group('DAYOFYEAR(registrants.created_at)')
+          .joins('INNER JOIN registrants ON registrations.registrant_id = registrants.id')
+          .where('faculty_id = ?', self.faculty_id)
+          .order('DAYOFYEAR(registrants.created_at) DESC')
+  end
+
+  def report_registrations
+    programmes = {}
+    self.openday_programmes.each do |programme|
+      programmes[programme.programme.name] = Registration.find(:all, 
+        select: 'count(*) count, DAYOFYEAR(created_at) as date', 
+        group: "DAYOFYEAR(created_at)", 
+        conditions: ['openday_id = ? AND programme_id = ?', self.openday_id, programme.programme_id], 
+        order: 'DAYOFYEAR(created_at) DESC'
+      )
+    end
+    return programmes
+  end
+
 end
