72fe0db4e1b3d6f01769321688ba6af4752f591c
Anrijs <ajargans@gmail.com>
2014-04-07 16:41:49 +0300

    Reports: added faculty comparison

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -118,6 +118,67 @@ class ReportsController < ApplicationController
       conditions: ['openday_id = ? OR openday_id = ?', @openday_a.id, @openday_b.id], 
       order: 'count DESC'
     )
+  end
 
+  def compare_faculty
+    @openday_a = Openday.find_by_slug(params[:report_id])
+    unless @openday_a
+      @openday_a = Openday.find(params[:report_id])
+    end
+
+    @openday_b = Openday.find_by_slug(params[:compare_id])
+    unless @openday_b
+      @openday_b = Openday.find(params[:compare_id])
+    end
+
+    @faculty = Faculty.find_by_slug(params[:id])
+    unless @faculty
+      @faculty = Faculty.find_by_id(params[:id])
+    end
+
+    if @faculty.nil?
+      redirect_to report_path(@openday_a)
+    else
+      @openday_faculty_a = @openday_a.openday_faculties.find_by_faculty_id(@faculty.id)
+      @openday_faculty_b = @openday_b.openday_faculties.find_by_faculty_id(@faculty.id)
+
+      @programme_registrations_a = {}
+      @openday_faculty_a.report_registrations.each do |name, registrations|
+        data = {}
+        registrations.each do |registration|
+          data[registration.date] = registration.count
+        end
+        @programme_registrations_a[name] = data
+      end
+
+      @programme_registrations_b = {}
+      @openday_faculty_b.report_registrations.each do |name, registrations|
+        data = {}
+        registrations.each do |registration|
+          data[registration.date] = registration.count
+        end
+        @programme_registrations_b[name] = data
+      end
+
+      @faculty_registrants_a = {}
+      @openday_faculty_a.report_registrants.each do |registrant|
+        @faculty_registrants_a[registrant.date] = registrant.count
+      end
+
+      @faculty_registrants_b = {}
+      @openday_faculty_b.report_registrants.each do |registrant|
+        @faculty_registrants_b[registrant.date] = registrant.count
+      end
+
+      @faculty_countries_a = @openday_faculty_a.report_countries
+      @faculty_countries_b = @openday_faculty_b.report_countries
+
+      @openday_countries = Registrant.find(:all, 
+      select: 'count(*) count, country', 
+      group: 'country', 
+      conditions: ['openday_id = ? OR openday_id = ?', @openday_a.id, @openday_b.id], 
+      order: 'count DESC'
+    )
+    end
   end
 end
